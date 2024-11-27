144e92839640dc46ce2085173c49daeea4261fc2
nancyyinan <xuey855@newschool.edu>
2024-11-26 19:42:49 -0500

    homepage grab developed

---
+++ entries/homepage/main.js
@@ -0,0 +1,33 @@
+$(document).ready(function () {
+  $("#candy1").draggable();
+});
+$(document).ready(function () {
+  $("#candy2").draggable();
+});
+$(document).ready(function () {
+  $("#candy3").draggable();
+});
+$(document).ready(function () {
+  $("#candy4").draggable();
+});
+$(document).ready(function () {
+  $("#candy5").draggable();
+});
+$(document).ready(function () {
+  $("#candy6").draggable();
+});
+$(document).ready(function () {
+  $("#candy7").draggable();
+});
+$(document).ready(function () {
+  $("#candy8").draggable();
+});
+$(document).ready(function () {
+  $("#candy9").draggable();
+});
+$(document).ready(function () {
+  $("#candy10").draggable();
+});
+$(document).ready(function () {
+  $("#candy11").draggable();
+});
